d4c294e157917ea17deb8a369ca5f0c44f5a0f4e
Zael-Eli <61130655+Zael-Eli@users.noreply.github.com>
2025-07-20 21:39:59 -0600

    start proyect

---
+++ src/main/java/com/dap/warehouse/DAPWarehouse/DapWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.dap.warehouse.DAPWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapWarehouseApplication.class, args);
+	}
+
+}
